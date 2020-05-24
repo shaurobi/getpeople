646db4217133aefb769e384fb5b8439b6a699aa9
Shaun Robinson <shaurobi@cisco.com>
2020-05-24 21:31:03 +1000

    Added comments

--- peoplecount.py
+++ peoplecount.py
@@ -8,6 +8,7 @@ def gatherPeople(endpoints):
         peopleCount(each)
         #insert database call later
 
+#edit peoplecount function later to include peoplecount
 def peopleCount(ip_address,hash_auth):
     url = "http://" + ip_address + "/getxml?location=/Status/Audio/Volume"
     payload = {}
@@ -15,6 +16,7 @@ def peopleCount(ip_address,hash_auth):
         'Authorization': 'Basic '+ hash_auth
     }
     response = requests.request("GET", url, headers=headers, data = payload, verify=False)
+    #tweak this request to get a 
     print(response.text.encode('utf8'))
 
-peopleCount("192.168.1.93", "YWRtaW46Y2lzY28xMjM=")
+peopleCount("192.168.1.93", "access token")
